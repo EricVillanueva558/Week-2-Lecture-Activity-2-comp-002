21ac602fdb48159edfae9621de2bf7bb353ab19f
Eric Villanueva <villanuevaeric1305@gmail.com>
2024-10-22 22:28:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let myGreeting = "Hello, welcome to JavaScript learning";
+
+console.log("myGreeting:", myGreeting);
+
+const pi = 3.14159;
+
+console.log("pi:", pi); // Output the value of pi
+
+let myNumber = 10;
+
+let minValue = Math.min(myNumber, pi);
+
+console.log("myNumber:", myNumber); // Output the value of myNumber
+console.log("Math.min(myNumber, pi):", minValue); // Output the result of Math.min
